1fb177482748b6a23bc13e71b20e7336a607319f
Yagmur Erdogan <yagmur.erdogan.1999@gmail.com>
2021-02-23 21:16:42 +0300

    Create custom component

---
+++ CustomButtons/app/src/main/java/com/yagmurerdogan/custombuttons/CustomButton.kt
@@ -0,0 +1,8 @@
+package com.yagmurerdogan.custombuttons
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class CustomButton(context: Context, attrs: AttributeSet) : View(context, attrs) {
+}
